789ac64d247ffc2d9dcd8dad06e71011c20893d5
sclecon <27941662@qq.com>
2022-04-11 17:51:32 +0800

    精简项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <el-button type="button">这是一个饿了么按钮</el-button>
+    <el-button type="button">EleMent Web Project</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
