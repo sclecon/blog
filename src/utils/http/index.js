bd8de4cc2146966ebef6fecaa81725ef9f9110bb
sclecon <27941662@qq.com>
2022-04-20 16:06:33 +0800

    优化封装axios请求库

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -5,22 +5,18 @@ import utils from './utils';
 
 const http = {
     request(mark, config={}){
-        const data = config.data || {};
-        const headers = config.headers || {};
-        const success = config.success || false;
-        const error = config.error || false;
         const api = utils.getApi(mark);
         if(typeof api === 'object'){
             const method = utils.getMethod(api);
-            const _config = {
-                params: utils.fusionParams(api, data),
-                headers: utils.fusionHeaders(api, headers)
-            };
+            const params = utils.fusionParams(api, config.data || {});
+            const headers = utils.fusionHeaders(api, config.headers || {});
+            const success = config.success || false;
+            const error = config.error || false;
             switch(method) {
-                case 'GET' :axios.get(api.path, _config).then(utils.then(success, error));break;
-                case 'POST' :axios.post(api.path, _config).then(utils.then(success, error));break;
-                case 'PUT' :axios.put(api.path, _config).then(utils.then(success, error));break;
-                case 'DELETE' :axios.delete(api.path, _config).then(utils.then(success, error));break;
+                case 'GET' :axios.get(api.path, {params, headers}).then(utils.then(success, error));break;
+                case 'POST' :axios.post(api.path, params, {headers}).then(utils.then(success, error));break;
+                case 'PUT' :axios.put(api.path, params, {headers}).then(utils.then(success, error));break;
+                case 'DELETE' :axios.delete(api.path, {params, headers}).then(utils.then(success, error));break;
             }
         }
     }
